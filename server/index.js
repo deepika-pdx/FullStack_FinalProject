30084c8115d809ed735c97d8b2f32236003a640b
Deepika Velapure <velapure@pdx.edu>
2022-11-14 22:18:09 -0800

    Added changes for MongoDB connection

--- server/index.js
+++ server/index.js
@@ -3,11 +3,13 @@
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const userdb = require("./mongodb/dailydairydb");
 
 app.get("/dashboard", (req, res) => {
   // res.status(200);
   // res.set({ "Content-Type": "text/html" });
   // res.send(`<h1>Welcome to Daily Dairy</h1>`);
+  userdb.dbConnect();
   res.json({ message: "Welcome to Daily Dairy!!" });
 });
 
