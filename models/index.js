428f9ec4161e13b6a669ef077258de8cca98dcea
gauri kasar <gaurikasar333@gmail.com>
2022-11-21 00:30:55 -0800

    changes for todo component

--- models/index.js
+++ models/index.js
@@ -1,10 +1,24 @@
 /** @format */
-
+const dotenv = require('dotenv').config();
+const cors = require('cors');
 const express = require("express");
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3003;
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+app.use(cors(corsOptions));
+
 const dailydiarydb = require("./mongodb/DailyDairyDb");
+const TodoItemRoute = require('./routes');
 
 app.listen(PORT, () => {
   console.log(`Server is listening on ${PORT}`);
 });
+app.use('/', TodoItemRoute);
--- models/mongodb/DailyDairyDb.js
+++ models/mongodb/DailyDairyDb.js
@@ -5,7 +5,7 @@ const express = require("express");
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
-const url = "mongodb+srv://dailyuser:DailyDairy565@dailydairycluster.oj0wdsh.mongodb.net/?retryWrites=true&w=majority";
+const url = "mongodb+srv://dailyuser:DailyDairy565@dailydairycluster.oj0wdsh.mongodb.net/DailyDatabase?retryWrites=true&w=majority";
 
 const connectionParams = {
   useNewUrlParser: true,
@@ -15,7 +15,7 @@ mongoose
   .connect(url, connectionParams)
   .then(() => {
     console.log("Connected to database ");
-    app.listen(3002, () => console.log("Server Up and running"));
+   // app.listen(3002, () => console.log("Server Up and running"));
   })
   .catch((err) => {
     console.error(`Error connecting to the database. \n${err}`);
