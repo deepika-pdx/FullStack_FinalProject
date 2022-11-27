47b6ab64580256cf6fc3d86ecf529817149cfc7c
Deepika Velapure <velapure@pdx.edu>
2022-11-26 19:33:26 -0800

    Commit changes for App

--- views/src/App.js
+++ views/src/App.js
@@ -12,6 +12,7 @@ function App() {
       {user && <Route path="/" exact element={<Login />} />}
       <Route path="/signup" exact element={<Signup />} />
       <Route path="/login" exact element={<Login />} />
+      <Route path="/" element={<Navigate replace to="/Main" />} />
     </Routes>
   );
 }
