29e3efdec332819daddadeb33722d5c0e84c2d50
Deepika Velapure <velapure@pdx.edu>
2022-11-26 19:31:57 -0800

    Resolving App and Main issue

--- views/src/App.js
+++ views/src/App.js
@@ -1,17 +1,4 @@
 /** @format */
-import "./App.css";
-import { useEffect } from "react";
-import { SplitPane } from "react-multi-split-pane";
-import TopBar from "./components/TopBar";
-import BottomBar from "./components/BottomBar";
-import LeftsideBar from "./components/LeftsideBar";
-import Content from "./components/Content";
-import RightsideBar from "./components/RightsideBar";
-import Exercise from "./components/Exercise";
-import FunActivities from "./components/FunActivities";
-import WaterTracker from "./components/WaterTracker";
-import full_glass_icon from "../src/images/WaterTracker/full_glass_icon.svg";
-import leg_exercise_1 from "../src/images/Exercise/leg_exercise_1.gif";
 
 import { Route, Routes, Navigate } from "react-router-dom";
 import Signup from "./components/Signup";
@@ -20,66 +7,12 @@ import Main from "./components/Main";
 
 function App() {
   const user = localStorage.getItem("token");
-  const waterTimeInterval = 60000;
-  const exerciseTimeInterval = 60000 * 2;
-  const waterReminderText = "Hey there!! Please drink enough water!";
-  const exerciseReminderText = "Hey there!! Try some relaxing exercises!";
-
-  useEffect(() => {
-    if (!("Notification" in window)) {
-      console.log("This browser does not support desktop notification");
-    } else {
-      Notification.requestPermission();
-    }
-
-    const waterInterval = setInterval(() => {
-      console.log("In water notification");
-      const waterOptions = {
-        body: waterReminderText,
-        icon: full_glass_icon,
-      };
-      new Notification("Water Reminder", waterOptions);
-    }, waterTimeInterval);
-
-    const exerciseInterval = setInterval(() => {
-      console.log("In exercisenotification");
-      const exerciseOptions = {
-        body: exerciseReminderText,
-        icon: leg_exercise_1,
-      };
-      new Notification("Exercise Reminder", exerciseOptions);
-    }, exerciseTimeInterval);
-
-    return () => {
-      clearInterval(waterInterval);
-      clearInterval(exerciseInterval);
-    };
-  }, []);
-
   return (
-    <React.Fragment>
-      <div className="App">
-        <SplitPane split="horizontal">
-          <TopBar />
-          <SplitPane split="vertical" className="splitPaneClass">
-            <LeftsideBar />
-            <Content />
-            <RightsideBar />
-          </SplitPane>
-          <SplitPane split="vertical" className="splitPaneClass">
-            <Exercise />
-            <WaterTracker />
-            <FunActivities />
-          </SplitPane>
-        </SplitPane>
-      </div>
-      <Routes>
-        {user && <Route path="/" exact element={<Login />} />}
-        <Route path="/signup" exact element={<Signup />} />
-        <Route path="/login" exact element={<Login />} />
-        <Route path="/" element={<Navigate replace to="/Main" />} />
-      </Routes>
-    </React.Fragment>
+    <Routes>
+      {user && <Route path="/" exact element={<Login />} />}
+      <Route path="/signup" exact element={<Signup />} />
+      <Route path="/login" exact element={<Login />} />
+    </Routes>
   );
 }
 
--- views/src/components/Main.js
+++ views/src/components/Main.js
@@ -1,22 +1,69 @@
+/** @format */
+
 import "../styles/Main.css";
+import { useEffect } from "react";
 import { SplitPane } from "react-multi-split-pane";
 import TopBar from "./TopBar";
-import BottomBar from "./BottomBar";
 import LeftsideBar from "./LeftsideBar";
 import Content from "./Content";
 import RightsideBar from "./RightsideBar";
+import Exercise from "./Exercise";
+import FunActivities from "./FunActivities";
+import WaterTracker from "./WaterTracker";
+import full_glass_icon from "../images/WaterTracker/full_glass_icon.svg";
+import leg_exercise_1 from "../images/Exercise/leg_exercise_1.gif";
 
 function Main() {
+  const waterTimeInterval = 60000;
+  const exerciseTimeInterval = 60000 * 2;
+  const waterReminderText = "Hey there!! Please drink enough water!";
+  const exerciseReminderText = "Hey there!! Try some relaxing exercises!";
+
+  useEffect(() => {
+    if (!("Notification" in window)) {
+      console.log("This browser does not support desktop notification");
+    } else {
+      Notification.requestPermission();
+    }
+
+    const waterInterval = setInterval(() => {
+      console.log("In water notification");
+      const waterOptions = {
+        body: waterReminderText,
+        icon: full_glass_icon,
+      };
+      new Notification("Water Reminder", waterOptions);
+    }, waterTimeInterval);
+
+    const exerciseInterval = setInterval(() => {
+      console.log("In exercisenotification");
+      const exerciseOptions = {
+        body: exerciseReminderText,
+        icon: leg_exercise_1,
+      };
+      new Notification("Exercise Reminder", exerciseOptions);
+    }, exerciseTimeInterval);
+
+    return () => {
+      clearInterval(waterInterval);
+      clearInterval(exerciseInterval);
+    };
+  }, []);
+
   return (
-    <div className="Main">
-      <SplitPane split="horizontal" minSize={50}>
+    <div className="App">
+      <SplitPane split="horizontal">
         <TopBar />
-        <SplitPane split="vertical">
+        <SplitPane split="vertical" className="splitPaneClass">
           <LeftsideBar />
           <Content />
           <RightsideBar />
         </SplitPane>
-        <BottomBar />
+        <SplitPane split="vertical" className="splitPaneClass">
+          <Exercise />
+          <WaterTracker />
+          <FunActivities />
+        </SplitPane>
       </SplitPane>
     </div>
   );
