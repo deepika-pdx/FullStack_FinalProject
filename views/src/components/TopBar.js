29bc6b7c582a042da1d25e57210d22d9a60571a8
Deepika Velapure <velapure@pdx.edu>
2022-11-30 16:45:53 -0800

    Commit changes for mobile device for topbar

--- views/src/components/TopBar.js
+++ views/src/components/TopBar.js
@@ -1,91 +1,89 @@
+/** @format */
+
 import React, { useEffect, useState } from "react";
 import DateTime from "./DateTime";
 import axios from "axios";
-import Weather from './Weather';
-import '../styles/TopBar.css';
+import Weather from "./Weather";
+import "../styles/TopBar.css";
 import weather from "../images/Weather/Weather.png";
 
+export function TopBar() {
+  const user = localStorage.getItem("email");
 
-export function TopBar (){
-	const user=localStorage.getItem("email");
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("email");
+    window.location.reload();
+  };
 
-	const handleLogout = () => {
-		localStorage.removeItem("token");
-		localStorage.removeItem("email");
-		window.location.reload();
-	};
+  // State
+  const [data, setData] = useState([]);
 
-	// State
-	const [data, setData] = useState([]);
+  const getUser = async () => {
+    try {
+      const url = "http://localhost:3001/auth";
+      const { data } = await axios.get(url, { withCredentials: true });
+      setData(data.user._json);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-    const getUser = async () => {
-		try {
-			const url = "http://localhost:3001/auth";
-			const { data } = await axios.get(url, { withCredentials: true });
-			setData(data.user._json);
-		} catch (err) {
-			console.log(err);
-		}
-	};
+  useEffect(() => {
+    getUser();
+  }, []);
 
-	useEffect(() => {
-		getUser();
-	}, []);
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
 
-	const [lat, setLat] = useState([]);
-  	const [long, setLong] = useState([]);
-
-  	useEffect(() => {
+  useEffect(() => {
     const fetchData = async () => {
-      navigator.geolocation.getCurrentPosition(function(position) {
+      navigator.geolocation.getCurrentPosition(function (position) {
         setLat(position.coords.latitude);
         setLong(position.coords.longitude);
       });
 
-	  // Weather
-    await fetch(`https://api.openweathermap.org/data/2.5/weather/?lat=${lat}&lon=${long}&units=metric&APPID=0dcd57de15dc5818f6f4502b50eb8975`)
-      .then(res => res.json())
-      .then(result => {
-        setData(result)
-        console.log(result);
-      });
-    }
+      // Weather
+      await fetch(
+        `https://api.openweathermap.org/data/2.5/weather/?lat=${lat}&lon=${long}&units=metric&APPID=0dcd57de15dc5818f6f4502b50eb8975`
+      )
+        .then((res) => res.json())
+        .then((result) => {
+          setData(result);
+          console.log(result);
+        });
+    };
     fetchData();
-  }, [lat,long])
+  }, [lat, long]);
 
-  	return (
-	<>
-	<div className="topBar" data-topbar role="navigation">
-	<div>
-    <img src={weather} alt="weather" width="50" height="50"></img>
-  </div>
-		<ul className="title-area">
-			<ul className="name">
-				<h1 className="Title"><b>DAILY DIARY</b></h1>
-				<p className="loggedInUser fixed-top"><b>Logged In User: {user}</b></p>
-				<button className="logOutBtn" onClick={handleLogout}>
-					Logout
-				</button>
-				<div>
-					
-				</div>
-			</ul>
-		</ul>
-	<div>
-		<div><DateTime></DateTime> </div>
-	<div>{(typeof data.main != 'undefined') ? (
-						<Weather weatherData={data} />
-					) : (
-						<div></div>
-		)}
-	</div>  
-	
-	</div>
-	</div></>
+  return (
+    <>
+      <div className="topBar" data-topbar role="navigation">
+        <div>
+          <img src={weather} alt="weather" width="50" height="50"></img>
+        </div>
+        <ul className="title-area">
+          <ul className="name">
+            <h1 className="Title">
+              <b>DAILY DIARY</b>
+            </h1>
+            <p className="loggedInUser fixed-top">
+              <b>Logged In User: {user}</b>
+            </p>
+            <button className="logOutBtn" onClick={handleLogout}>
+              Logout
+            </button>
+            <div></div>
+          </ul>
+        </ul>
+        <div>
+          <div>
+            <DateTime></DateTime>{" "}
+          </div>
+          <div>{typeof data.main != "undefined" ? <Weather weatherData={data} /> : <div></div>}</div>
+        </div>
+      </div>
+    </>
   );
 }
 export default TopBar;
-
-
-
-
