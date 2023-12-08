5cdd92f9db2cbd0f248ce3409df03c665d53d97e
Swetha <swetha@Swethas-MBP.T-mobile.com>
2023-12-08 15:06:55 -0800

    added comments to a few files

--- views/src/components/DateTime.js
+++ views/src/components/DateTime.js
@@ -5,6 +5,7 @@ export const DateTime = () => {
   var [date, setDate] = useState(new Date());
 
   useEffect(() => {
+    // Setting up an interval to update the date every 1000 milliseconds (1 second)
     var timer = setInterval(() => setDate(new Date()), 1000);
     return function cleanup() {
       clearInterval(timer);
--- views/src/components/EventsDisplay.js
+++ views/src/components/EventsDisplay.js
@@ -1,3 +1,4 @@
+// Component for displaying events in an accordion format
 import React, { useState } from 'react';
 import '../styles/NearbyEvents.css';
 
@@ -8,10 +9,19 @@ const EventsDisplay = ({ title, content, fields }) => {
     <div className="accordion-item">
       <div className="accordion-title" onClick={() => setIsActive(!isActive)}>
         <div>{title}</div>
-        <div><b>{isActive ? '-' : '+'}</b></div>
+        <div>
+          <b>{isActive ? '-' : '+'}</b>
+        </div>
       </div>
       {isActive && <div className="accordion-content">{content}</div>}
-      {isActive && <div className="accordion-content">{fields}  <a href="https://www.travelportland.com/events/search/?start=2022-12-10&end=2022-12-30&type=">Click Here To Know More</a></div>}
+      {isActive && (
+        <div className="accordion-content">
+          {fields}{' '}
+          <a href="https://www.travelportland.com/events/search/?start=2022-12-10&end=2022-12-30&type=">
+            Click Here To Know More
+          </a>
+        </div>
+      )}
     </div>
   );
 };
